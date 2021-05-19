5e1aedcc4fdfd51507a7817d5524f05572b6211a
jotasmk <jota.smk@gmail.com>
2021-05-19 12:25:38 -0300

    cleanning

--- feneb.f90
+++ feneb.f90
@@ -93,22 +93,9 @@ logical ::  per, velin, velout, relaxd, converged, wgrad, wtemp, moved, maxpreac
     write(9999,*) "---------------------------------------------------"
     write(9999,*) "       Performing NEB on the FE surface"
     write(9999,*) "---------------------------------------------------"
-!------------ Get coordinates for previously optimized extrema
-!------------ And set forces to zero
 
+!------------ Set forces to zero
 
-    ! !Reactants
-    ! call getfilenames(1,chi,infile,reffile,outfile,iname,rname,oname)
-    ! call getrefcoord(rname,nrestr,mask,natoms,rref,boxinfo,per,velin)
-    ! call getcoordextrema(rref,natoms,rav,nrestr,nrep,1,mask)
-    !
-    !
-    ! !Products
-    ! call getfilenames(nrep,chi,infile,reffile,outfile,iname,rname,oname)
-    ! call getrefcoord(rname,nrestr,mask,natoms,rref,boxinfo,per,velin)
-    ! call getcoordextrema(rref,natoms,rav,nrestr,nrep,nrep,mask)
-
-    !Forces set to zero
     fav=0.d0
 
 !------------ Band loop
@@ -169,149 +156,42 @@ logical ::  per, velin, velout, relaxd, converged, wgrad, wtemp, moved, maxpreac
     do i=start,end
 
       call getfilenames(i,chi,infile,reffile,outfile,iname,rname,oname) !rname = NAME_r_i.rst7 ; i=replica
-      !-----------------test
       call getrefcoord(rname,nrestr,mask,natoms,rref,boxinfo,per,velin)
-      ! if (i .le. 9) write(chi,'(I1)') i
-      ! if (i .gt. 9 .and. i .le. 99) write(chi,'(I2)') i
-      ! if (i .gt. 99 .and. i .le. 999) write(chi,'(I3)') i
-      ! tempname = "testrrefall"// trim(chi)
-      ! tempname = trim(tempname) // ".rst7"
-      ! call writenewcoord(tempname,rref,boxinfo,natoms,nrestr,mask,per,velout,rav,nrep,i,test)
-      !----------------tests
       call getcoordextrema(rref,natoms,rrefall,nrestr,nrep,i,mask)
-!      call getcoordextrema(rref,natoms,rav,nrestr,nrep,1,mask)
 
     end do
     test=.False.
-    ! allocate(selfdist(2,nrestr,nrep-1))
-    !
-    ! call getselfdist(rav, rrefall, nrep, nrestr, selfdist)
-    !
-    ! open(unit=1648, file="selfdist_rrefCHECK.dat")
-    ! do i=1,nrestr
-    !   do n=1,nrep-1
-    !     write(1648,'(2x, I6,2x, f20.10)') n, selfdist(1,i,n)
-    !   end do
-    !   write(1648,*)
-    ! end do
-    ! close(1648)
-    ! do k=1,nrep
-    ! do i=1,nrestr
-    ! write(*,'(2x, I6,2x, 3(f20.10,2x))') i, rrefall(1:3,i,k)
-    ! end do
-    ! end do
-
 
 !----------- Compute the free energy profile by umbrella integration
     allocate(profile(2,nrep-1))
-!----------- Computes tangent and nebforce
-    write(9999,*) "-----------------------------------------------------"
-    write(9999,*) "r, tg, f points: ", rpoint, tgpoint, fpoint
-    write(9999,*) "-----------------------------------------------------"
-    if ((rpoint .eq. 0) .and. (tgpoint .eq. 0) .and. (fpoint .eq. 0)) then
-      call getprofile(rav,fav,nrep,nrestr,profile)
-      call gettang(rav,tang,nrestr,nrep)
-      call getnebforce(rav,fav,tang,nrestr,nrep,kspring,maxforceband,ftol,relaxd,&
-                          ftrue,ftang,fperp,fspring,.true.,dontg)
-    elseif ((rpoint .eq. 1) .and. (tgpoint .eq. 0) .and. (fpoint .eq. 0)) then
-      call getprofile(rav,fav,nrep,nrestr,profile)
-      call gettang(rav,tang,nrestr,nrep)
-      call getnebforce(rav,fav,tang,nrestr,nrep,kspring,maxforceband,ftol,relaxd,&
-                          ftrue,ftang,fperp,fspring,.true.,dontg)
-      rav=rrefall
-    elseif ((rpoint .eq. 0) .and. (tgpoint .eq. 1) .and. (fpoint .eq. 0)) then
-      call getprofile(rav,fav,nrep,nrestr,profile)
-      call gettang(rrefall,tang,nrestr,nrep)
-      call getnebforce(rav,fav,tang,nrestr,nrep,kspring,maxforceband,ftol,relaxd,&
-                          ftrue,ftang,fperp,fspring,.true.,dontg)
-    elseif ((rpoint .eq. 1) .and. (tgpoint .eq. 1) .and. (fpoint .eq. 0)) then
-      call getprofile(rav,fav,nrep,nrestr,profile)
-      call gettang(rrefall,tang,nrestr,nrep)
-      call getnebforce(rav,fav,tang,nrestr,nrep,kspring,maxforceband,ftol,relaxd,&
-                          ftrue,ftang,fperp,fspring,.true.,dontg)
-      rav=rrefall
-    elseif ((rpoint .eq. 1) .and. (tgpoint .eq. 1) .and. (fpoint .eq. 1)) then
-      call gettang(rrefall,tang,nrestr,nrep)
-      allocate(fonref(3,nrestr,nrep))
-      fonref=0.d0
-      do i=2,nrep-1
-        do j=1,nrestr
-          do k=1,3
-            fonref(k,j,i)=fav(k,j,i)*(1-((fav(k,j,i+1)-fav(k,j,i-1))/(rav(k,j,i+1)-rav(k,j,i-1)))*(1.d0/kref))
-          end do
-        end do
+    call getprofile(rav,fav,nrep,nrestr,profile)
+    call gettang(rav,tang,nrestr,nrep)
+    call getnebforce(rav,fav,tang,nrestr,nrep,kspring,maxforceband,ftol,relaxd,&
+                    ftrue,ftang,fperp,fspring,.true.,dontg)
+  ! fav ---> fneb
+
+
+
+
+
+    call getbarrier(profile, nrep, barrier, minpoint, maxpoint)
+
+    allocate(selfdist(2,nrestr,nrep-1))
+    call getselfdist(rav, rrefall, nrep, nrestr, selfdist)
+
+    open(unit=1644, file="selfdist_rref.dat")
+    open(unit=1645, file="selfdist_rav.dat")
+    do i=1,nrestr
+      do n=1,nrep-1
+        write(1644,'(2x, I6,2x, f20.10)') n, selfdist(1,i,n)
+        write(1645,'(2x, I6,2x, f20.10)') n, selfdist(2,i,n)
       end do
-      fav=fonref
-      call getnebforce(rrefall,fav,tang,nrestr,nrep,kspring,maxforceband,ftol,relaxd,&
-                          ftrue,ftang,fperp,fspring,.true.,dontg)
-      call getprofile(rrefall,fonref,nrep,nrestr,profile)
-      rav=rrefall
-     elseif ((rpoint .eq. 2) .and. (tgpoint .eq. 2) .and. (fpoint .eq. 2)) then
-      call gettang(rrefall,tang,nrestr,nrep)
-      do i=2,nrep-1
-        do j=1,nrestr
-          do k=1,3
-            rcorr(k,j,i)=((rav(1,j,i)-rrefall(1,j,i))*tang(1,j,i)+&
-                         (rav(2,j,i)-rrefall(2,j,i))*tang(2,j,i)+&
-                         (rav(3,j,i)-rrefall(3,j,i))*tang(3,j,i))*tang(k,j,i)
-          end do
-        end do
-      end do
-      rcorr=rav+rcorr
-      allocate(fonref(3,nrestr,nrep))
-      fonref=0.d0
+    write(1644,*)
+    write(1645,*)
+    end do
+    close(1644)
+    close(1645)
 
-      do i=2,nrep-1
-        do j=1,nrestr
-          do k=1,3
-              fonref(k,j,i)=fav(k,j,i)+((fav(k,j,i+1)-fav(k,j,i-1))/(rav(k,j,i+1)-rav(k,j,i-1)))*(rcorr(k,j,i)-rav(k,j,i))
-          end do
-        end do
-      end do
-      ! fonref=fav
-      ! write(*,*) fonref
-      fav=fonref
-      call getprofile(rcorr,fonref,nrep,nrestr,profile)
-      call gettang(rcorr,tang,nrestr,nrep)
-      call getnebforce(rcorr,fav,tang,nrestr,nrep,kspring,maxforceband,ftol,relaxd,&
-                          ftrue,ftang,fperp,fspring,.true.,dontg)
-      rav=rcorr
-     else
-       write(9999,*) "Combination of r,tg and f point not allowed"
-       STOP
-     end if
-
-     call getbarrier(profile, nrep, barrier, minpoint, maxpoint)
-
-     allocate(selfdist(2,nrestr,nrep-1))
-     call getselfdist(rav, rrefall, nrep, nrestr, selfdist)
-
-     open(unit=1644, file="selfdist_rref.dat")
-     open(unit=1645, file="selfdist_rav.dat")
-     do i=1,nrestr
-       do n=1,nrep-1
-         write(1644,'(2x, I6,2x, f20.10)') n, selfdist(1,i,n)
-         write(1645,'(2x, I6,2x, f20.10)') n, selfdist(2,i,n)
-       end do
-       write(1644,*)
-       write(1645,*)
-     end do
-     close(1644)
-     close(1645)
-
-
-     ! open(unit=16450, file="temp.dat")
-
-     ! do j=1,tempfilesize
-     !   write(*,*) temp(j,1:nrep-2)
-     ! end do
-
-     ! if (tgpoint .eq. 2) call gettang(rcorr,tang,nrestr,nrep)
-
-
-     ! if (fpoint .eq. 2) call getnebforce(rav,fav,tang,nrestr,nrep,kspring,maxforceband,ftol,relaxd,&
-                      ! ftrue,ftang,fperp,fspring,.true.,dontg)
-! fav ---> fneb
 
 !----------- moves the band
     if (.not. converged) then
@@ -401,13 +281,11 @@ logical ::  per, velin, velout, relaxd, converged, wgrad, wtemp, moved, maxpreac
         call getrefcoord(rname,nrestr,mask,natoms,rref,boxinfo,per,velin)
         call getcoordextrema(rref,natoms,rav,nrestr,nrep,1,mask)
         call writenewcoord(oname,rref,boxinfo,natoms,nrestr,mask,per,velout,rav,nrep,1,test)
-        !
-        ! !Products
+        !Products
         call getfilenames(nrep,chi,infile,reffile,outfile,iname,rname,oname)
         call getrefcoord(rname,nrestr,mask,natoms,rref,boxinfo,per,velin)
         call getcoordextrema(rref,natoms,rav,nrestr,nrep,nrep,mask)
         call writenewcoord(oname,rref,boxinfo,natoms,nrestr,mask,per,velout,rav,nrep,nrep,test)
-        !
     end if
         do i=2,nrep-1
           ! call getfilenames(i,chi,infile,reffile,outfile,iname,rname,oname)
